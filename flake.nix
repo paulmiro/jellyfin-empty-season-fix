d7f431b759afa2bf302c4c3c11da8bf9c8d970db
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-08-02 00:31:15 +0200

    setup flake, envrc and main.py

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ python3 ] ++
+            (with pkgs.python3Packages; [
+              pip
+              venvShellHook
+              jellyfin-apiclient-python
+            ]);
+        };
+      });
+    };
+}
---
+++ main.py
@@ -0,0 +1,9 @@
+from jellyfin_apiclient_python import JellyfinClient
+
+
+def main():
+    client = JellyfinClient()
+
+
+if __name__ == "__main__":
+    main()
