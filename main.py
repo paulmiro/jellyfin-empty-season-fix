9ec745aac45fed98242647b7f6c80beee0767113
paulmiro <30203227+paulmiro@users.noreply.github.com>
2024-08-02 23:16:22 +0200

    extract setup() method

--- main.py
+++ main.py
@@ -5,14 +5,7 @@ import urllib3
 
 
 def main():
-    config = json.load(open("config.local.json"))
-    global AUTH_HEADERS, URL, VERYFY_SSL
-    AUTH_HEADERS = {"Accept": "application/json", "X-Emby-Token": config["api_token"]}
-    URL = config["jellyfin_host"]
-    VERYFY_SSL = config["verify_ssl"]
-
-    if not VERYFY_SSL:
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    setup()
 
     virtual_episodes_result = get_virtual_episodes()
 
@@ -37,6 +30,17 @@ def main():
             print(
                 f"{counter} virtual episodes in Season {season['Name']} in Series {season['SeriesName']}"
             )
+
+
+def setup():
+    config = json.load(open("config.local.json"))
+    global AUTH_HEADERS, URL, VERYFY_SSL
+    AUTH_HEADERS = {"Accept": "application/json", "X-Emby-Token": config["api_token"]}
+    URL = config["jellyfin_host"]
+    VERYFY_SSL = config["verify_ssl"]
+
+    if not VERYFY_SSL:
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def get_virtual_episodes():
